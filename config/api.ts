4028d22405d7c06b73f30af5372c8e535cad0d87
GustavoMont <lgmluisgm@gmail.com>
2023-11-12 22:52:36 -0300

    fix: update enviroment variables

--- config/api.ts
+++ config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DOMAIN = process.env.VERCEL_URL ?? "http://localhost:3000";
+const DOMAIN = process.env.NEXT_PUBLIC_API ?? "http://localhost:3000";
 
 const baseURL = `${DOMAIN}/api`;
 
